cc5c8ca82944ff9430293f6c031d57bb301d14f1
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-20 16:36:19 -0300

    colocando cor de fundo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Pedometer } from "expo-sensors";
 
 export default function App() {
@@ -10,10 +10,15 @@ export default function App() {
 
   useEffect(() => {
     // Função assíncrona para verificar a disponibilidade do pedômetro e obter os dados de contagem de passos
+    // O resultado da verificação de disponibilidade é armazenado na variável isAvailable. Se o pedômetro estiver disponível, isAvailable será true; caso contrário, será false.
     const subscribe = async () => {
       const isAvailable = await Pedometer.isAvailableAsync();
       setIsPedometerAvailable(String(isAvailable)); // Define a disponibilidade do pedômetro
+      // Aqui, estamos usando a função isAvailableAsync() do módulo Pedometer do pacote expo-sensors. Esta função verifica se o pedômetro está disponível no dispositivo atual.
+      //       isPedometerAvailable é um estado que armazena a string que indica se o pedômetro está disponível. Inicialmente, estamos definindo o estado como "checking".
+      // Usamos String(isAvailable) para converter o valor booleano isAvailable em uma string, porque queremos atualizar o estado com uma string representando o status da disponibilidade do pedômetro.
 
+      // if (String(isAvailable)); definiu isPedometerAvailable como true
       if (isAvailable) {
         const end = new Date();
         const start = new Date();
@@ -46,14 +51,24 @@ export default function App() {
 
   // Renderiza a interface do usuário
   return (
-    <View style={styles.container}>
-      {/* Exibe a disponibilidade do pedômetro */}
-      <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
-      {/* Exibe a contagem de passos do período anterior */}
-      <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
-      {/* Exibe a contagem de passos atual */}
-      <Text>Walk! And watch this go up: {currentStepCount}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.blueSection}>
+        {/* Exibe a disponibilidade do pedômetro */}
+        <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
+        {/* Exibe a contagem de passos do período anterior */}
+        <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
+        {/* Exibe a contagem de passos atual */}
+        <Text>Walk! And watch this go up: {currentStepCount}</Text>
+      </View>
+      <View style={styles.whiteSection}>
+        {/* Exibe a disponibilidade do pedômetro */}
+        <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
+        {/* Exibe a contagem de passos do período anterior */}
+        <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
+        {/* Exibe a contagem de passos atual */}
+        <Text>Walk! And watch this go up: {currentStepCount}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -64,5 +79,22 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
+  },
+  blueSection: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: "66%", // 1/3 da altura da tela
+    backgroundColor: "#000326", // Azul marinho
+    color: "white",
+  },
+  whiteSection: {
+    position: "absolute",
+    top: "33%", // 1/3 da altura da tela
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "#FFFFFF", // Branco
   },
 });
