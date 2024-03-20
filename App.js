d963e7647584670894e96976be503e02d2e29086
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-20 18:17:24 -0300

    finalizando estilização

--- App.js
+++ App.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Pedometer } from "expo-sensors";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
-  // Estados para armazenar informações do pedômetro
   const [isPedometerAvailable, setIsPedometerAvailable] = useState("checking");
   const [pastStepCount, setPastStepCount] = useState(0);
   const [currentStepCount, setCurrentStepCount] = useState(0);
 
   useEffect(() => {
-    // Função assíncrona para verificar a disponibilidade do pedômetro e obter os dados de contagem de passos
-    // O resultado da verificação de disponibilidade é armazenado na variável isAvailable. Se o pedômetro estiver disponível, isAvailable será true; caso contrário, será false.
     const subscribe = async () => {
       const isAvailable = await Pedometer.isAvailableAsync();
-      setIsPedometerAvailable(String(isAvailable)); // Define a disponibilidade do pedômetro
-      // Aqui, estamos usando a função isAvailableAsync() do módulo Pedometer do pacote expo-sensors. Esta função verifica se o pedômetro está disponível no dispositivo atual.
-      //       isPedometerAvailable é um estado que armazena a string que indica se o pedômetro está disponível. Inicialmente, estamos definindo o estado como "checking".
-      // Usamos String(isAvailable) para converter o valor booleano isAvailable em uma string, porque queremos atualizar o estado com uma string representando o status da disponibilidade do pedômetro.
-
-      // if (String(isAvailable)); definiu isPedometerAvailable como true
+      setIsPedometerAvailable(String(isAvailable));
       if (isAvailable) {
         const end = new Date();
         const start = new Date();
         start.setDate(end.getDate() - 1);
 
-        // Obtém a contagem de passos do período anterior
         const pastStepCountResult = await Pedometer.getStepCountAsync(
           start,
           end
@@ -33,12 +31,10 @@ export default function App() {
           setPastStepCount(pastStepCountResult.steps); // Define a contagem de passos anterior
         }
 
-        // Inicia a assinatura para obter a contagem de passos em tempo real
         const subscription = Pedometer.watchStepCount((result) => {
           setCurrentStepCount(result.steps); // Atualiza a contagem de passos atual
         });
 
-        // Define a função de limpeza para remover a assinatura quando o componente for desmontado
         return () => subscription && subscription.remove();
       }
     };
@@ -49,52 +45,91 @@ export default function App() {
     return () => subscription && subscription.remove();
   }, []);
 
-  // Renderiza a interface do usuário
+  // Função para reiniciar a contagem de passos
+  const handleResetSteps = () => {
+    setPastStepCount(0);
+    setCurrentStepCount(0);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.blueSection}>
-        {/* Exibe a disponibilidade do pedômetro */}
+      <View style={styles.areaAzul}>
         <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
-        {/* Exibe a contagem de passos do período anterior */}
-        <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
-        {/* Exibe a contagem de passos atual */}
-        <Text>Walk! And watch this go up: {currentStepCount}</Text>
+        <Text style={styles.progresso}>Progresso de Hoje</Text>
+        <Text style={styles.dateText}>{new Date().toLocaleDateString()}</Text>
+
+        <Text style={styles.stepCount}>
+          {currentStepCount}
+          <Ionicons name="footsteps" size={40} color="white" />
+        </Text>
+        <Text style={styles.stepLabel}>Passos</Text>
+        {/* <Text>Steps taken in the last 24 hours: {pastStepCount}</Text> */}
       </View>
       <View style={styles.whiteSection}>
-        {/* Exibe a disponibilidade do pedômetro */}
-        <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
-        {/* Exibe a contagem de passos do período anterior */}
-        <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
-        {/* Exibe a contagem de passos atual */}
-        <Text>Walk! And watch this go up: {currentStepCount}</Text>
+        <TouchableOpacity
+          onPress={handleResetSteps}
+          style={styles.botaoResetar}
+        >
+          <Text style={styles.botaoResetarText}>Reiniciar Contagem</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 }
 
-// Estilos para o componente
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#ffffff",
   },
-  blueSection: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: "66%", // 1/3 da altura da tela
-    backgroundColor: "#000326", // Azul marinho
-    color: "white",
+  areaAzul: {
+    width: "100%",
+    height: "60%",
+    marginTop: 0,
+    backgroundColor: "#77A476",
+    alignItems: "center",
+    justifyContent: "center",
   },
   whiteSection: {
-    position: "absolute",
-    top: "33%", // 1/3 da altura da tela
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "#FFFFFF", // Branco
+    alignItems: "center",
+    marginTop: 7,
+    // justifyContent: "center" ,
+  },
+  dateText: {
+    fontSize: 18,
+    marginBottom: 42,
+    color: "white",
+  },
+  progresso: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+  },
+  stepCount: {
+    fontSize: 70,
+    color: "white",
+    marginBottom: 22,
+    fontWeight: "bold",
+  },
+  stepLabel: {
+    fontSize: 24,
+    color: "white",
+    fontWeight: "bold",
+  },
+
+  botaoResetar: {
+    marginTop: 20,
+    backgroundColor: "#EF5757",
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    overflow: "hidden",
+  },
+  botaoResetarText: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "bold",
   },
 });
